8e7e334141ad67e1828f056549857c2150f9cace
mohabmalah <43172048+mohabmalah@users.noreply.github.com>
2018-09-11 14:23:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,17 +53,6 @@ client.on('message' , async (message) => {
     }
 });
   
-
-
- function codes() {
-       this.clear = function(message) {
-           const args = message.content.slice(config.prefix.length).trim().split(/ +/g); const command = args.shift().toLowerCase();
-if(command == "clear") {
-message.delete() if(!message.channel.guild) return; if(message.member.hasPermission(0x2000)){ let count = args[0] || 100; let messagecount = parseInt(count); message.channel.fetchMessages({limit: messagecount}) .then( messages => { count = messages.array().length message.channel.send(` ** messages deleted count: ${count} **`).then(m => m.delete(2500)); message.delete(60000); return messages; }) .then(messages => message.channel.bulkDelete(messages)) } else { var manage = new Discord.RichEmbed() .setDescription('You Do Not Have Permission to use this') .setColor("RANDOM") message.channel.send(manage) return;
-}
-       }
-}
-
 module.exports = codes;
 
  client.on('message', message => {
