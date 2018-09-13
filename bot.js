82519c75211147517119f506f6d8ad7e226a74b8
mohabmalah <43172048+mohabmalah@users.noreply.github.com>
2018-09-13 20:06:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -303,6 +303,87 @@ client.on('message', message => {//restart
 });
 
 
+rn = {};
+
+var user = {};
+var warn = {};
+
+client.on('message', function(message) {
+
+    	 if (!message.channel.guild) return;
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+
+  if (message.author.id == client.user.id) return;
+  if(JSON.stringify(user).indexOf(message.author.id) == -1) {
+    user[message.author.id] = message.createdTimestamp;
+    return;
+  } else {
+    if (Date.now() - user[message.author.id] < 695){
+              message.author.delete
+
+      if (JSON.stringify(warn).indexOf(message.author.id) == -1) {
+        warn[message.author.id] = 1;
+      } else {
+        warn[message.author.id]++;
+        message.author.delete
+      }
+      if (warn[message.author.id] < 4) {
+        message.author.delete
+
+      }
+      delete user[message.author.id];
+              message.author.delete
+
+    } else {
+      delete user[message.author.id];
+              message.author.delete
+
+    }
+  }
+  if (warn[message.author.id] == 4) {		   
+     if (!message.channel.guild) return;
+             message.author.delete
+
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+          var currentTime = new Date(),
+                   Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate(),
+hours = currentTime.getHours() + 3 ,
+            minutes = currentTime.getMinutes()+1,
+            seconds = currentTime.getSeconds();
+
+           if (!message.channel.guild) return;
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+    message.guild.members.get(message.author.id).addRole(muteRole1);
+    
+     var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+
+delete warn[message.author.id];
+    delete user[message.author.id];
+	const embed500 = new Discord.RichEmbed()
+     .setTitle(`ÇáãÑÓá ${message.author.username}#${message.author.discriminator} `)
+      .setDescription(":white_check_mark:  | `ãÍÇæáÉ ÇáÓÈÇã`\n\nÇáÇÓã:\n"+`${message.author.username}#${message.author.discriminator}`+"\nÇáÚÞæÈÉ:\nãíæÊ ÔÇÊ\n")
+          .setFooter("KiNg66S")
+      .setColor("c91616")
+    message.channel.send(embed500)
+    	const embed20 = new Discord.RichEmbed()
+      .setTitle(":scales: | ÊãÊ ãÚÇÞÈÊß")
+      .setDescription(`**áÞÏ ÞãÊ ÈãÎÇáÝÉ ÞæÇäíä ÇáÓíÑÝÑ**\n\nÊãÊ ãÚÇÞÈÊß ãä ÞÈá :\nL-RO3B BOT\näæÚ ÇáÚÞæÈÉ:\nãíæÊ ÔÇÊ\nÊÇÑíÎ ÇáÚÞæÈÉ:\n`+ Year + "/" + Month + "/" + Day +', '+hours +'-' +minutes+'-'+seconds+"\n \n \n`Ýí ÍÇá ßÇäÊ ÇáÚÞæÈÉ ÈÇáÛáØ, ÊæÇÕá ãÚ ÇáÇÏÇÑÉ`")
+          .setFooter("KiNg66S")
+      .setColor("c91616")
+    
+     message.author.send(embed20)
+  
+  }
+});
 
 
 
